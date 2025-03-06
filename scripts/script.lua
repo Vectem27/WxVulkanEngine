09d81ffdb445d20623c34a01efc351eaa8a475c2
Vectem27 <bournatmv@gmail.com>
2025-03-06 04:37:06 +0100

    Add working wxLua script with wx dependancies

--- scripts/script.lua
+++ scripts/script.lua
@@ -3,10 +3,12 @@
 --local frame = wx.wxFrame(wx.NULL, wx.wxID_ANY, "Lua Frame")
 --frame:Show(true)
 
-function retourner_nombre()
-    return 42  -- Le nombre retourné peut être modifié selon tes besoins
-end
+package.cpath = package.cpath..";./?.dll;./?.so;../lib/?.so;../lib/vc_dll/?.dll;../lib/bcc_dll/?.dll;../lib/mingw_dll/?.dll;"
+print("Démarrage de l'application")
 
--- Exemple d'appel de la fonction
-local nombre = retourner_nombre()
-print(nombre)
+local wx = require("wx")
+
+print("Module wx chargé")
+
+local frame = wx.wxFrame(wx.NULL, wx.wxID_ANY, "Lua Frame")
+frame:Show(true)
--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,86 @@
 #include <wx/wx.h>
+#include <vulkan/vulkan.h>
 #include <wxlua/wxlua.h>
+#include <stdexcept>
+#include "wxlua/wxlstate.h"
 
-class MyApp : public wxApp
-{
+#include "wxbind/include/wxbinddefs.h"
+WXLUA_DECLARE_BIND_WXCORE
+WXLUA_DECLARE_BIND_WXBASE
+
+
+
+class MyApp : public wxApp {
 public:
     virtual bool OnInit();
-};
-
-class MyFrame : public wxFrame
-{
-public:
-    MyFrame(const wxString& title);
-
-    void OnQuit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
 
 private:
-    wxLuaState m_wxlState;
+};
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame(const wxString& title);
+    ~MyFrame();
+
+private:
+    VkInstance vulkanInstance;
 };
 
 wxIMPLEMENT_APP(MyApp);
 
-bool MyApp::OnInit()
+bool MyApp::OnInit() 
 {
-    if (!wxApp::OnInit())
-        return false;
 
-    MyFrame *frame = new MyFrame("wxLua Example");
+    WXLUA_IMPLEMENT_BIND_WXCORE
+    WXLUA_IMPLEMENT_BIND_WXBASE
+    MyFrame *frame = new MyFrame("Vulkan wxWidgets App");
     frame->Show(true);
+
+    wxLuaState luaState(true);
+    if (!luaState.Ok()) {
+        wxMessageBox("Échec de l'initialisation de wxLua.", "Erreur", wxOK | wxICON_ERROR);
+        return false;
+    }
+
+    try 
+    {
+        if (int rc = luaState.RunFile(wxT("scripts/script.lua"))) {
+            wxMessageBox(wxlua_LUA_ERR_msg(rc), "Erreur", wxOK | wxICON_ERROR);
+            return false;
+        }
+    }
+    catch( ... )
+    {
+        wxMessageBox("Catch error", "Erreur", wxOK | wxICON_ERROR);
+    }
+    
 
     return true;
 }
 
 MyFrame::MyFrame(const wxString& title)
-    : wxFrame(NULL, wxID_ANY, title), m_wxlState(true)
-{
-    wxMenu *fileMenu = new wxMenu;
-    fileMenu->Append(wxID_EXIT, "&Quit\tCtrl+Q", "Quit the application");
+    : wxFrame(NULL, wxID_ANY, title), vulkanInstance(VK_NULL_HANDLE) {
+    // Initialisation de Vulkan
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan wxWidgets App";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    wxMenu *helpMenu = new wxMenu;
-    helpMenu->Append(wxID_ABOUT, "&About\tF1", "Show about dialog");
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
 
-    wxMenuBar *menuBar = new wxMenuBar();
-    menuBar->Append(fileMenu, "&File");
-    menuBar->Append(helpMenu, "&Help");
-
-    SetMenuBar(menuBar);
-
-    Bind(wxEVT_MENU, &MyFrame::OnQuit, this, wxID_EXIT);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-
-    // Initialise wxLua
-    if (m_wxlState.Ok())
-    {
-        
-        m_wxlState.RunString(wxT("print(\"Test\")"), wxT("=(command line)"));
-        m_wxlState.RunFile("scripts/script.lua");
-    }
-    else
-    {
-        wxMessageBox("Failed to initialise wxLua", "Error", wxOK | wxICON_ERROR, this);
+    if (vkCreateInstance(&createInfo, nullptr, &vulkanInstance) != VK_SUCCESS) {
+        wxLogError("Failed to create Vulkan instance!");
+        return;
     }
 }
 
-void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
-{
-    Close(true);
-}
-
-void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
-{
-    wxMessageBox("This is a wxLua example", "About wxLua", wxOK | wxICON_INFORMATION, this);
+MyFrame::~MyFrame() {
+    if (vulkanInstance != VK_NULL_HANDLE) {
+        vkDestroyInstance(vulkanInstance, nullptr);
+    }
 }
