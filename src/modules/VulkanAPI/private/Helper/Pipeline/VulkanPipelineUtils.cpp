c327250c647cebeccb01070704ef20cbc6192c12
Vectem <vectem@okarana.fr>
2025-04-16 02:34:36 +0200

    Create doxygen doc comment and move definition to cpp file

--- src/modules/VulkanAPI/private/Helper/Pipeline/VulkanPipelineUtils.cpp
+++ src/modules/VulkanAPI/private/Helper/Pipeline/VulkanPipelineUtils.cpp
@@ -1,1 +1,125 @@
 #include "VulkanPipelineUtils.h"
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+#include "Logger.h"
+
+#include "VulkanDeviceManager.h"
+
+using namespace VulkanPipelineUtils;
+
+VkShaderModule VulkanPipelineUtils::CreateShaderModule(const VulkanShaderInfo& shaderInfo) 
+{
+    VkShaderModule shaderModule = VK_NULL_HANDLE;
+
+    if (shaderInfo.shaderCode == nullptr || shaderInfo.shaderCodeSize <= 0) 
+    {
+        return VK_NULL_HANDLE;
+    }
+
+    VkShaderModuleCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.codeSize = shaderInfo.shaderCodeSize;
+    createInfo.flags = shaderInfo.flags;
+    createInfo.pCode = reinterpret_cast<uint32_t*>(shaderInfo.shaderCode);
+    createInfo.pNext = nullptr;
+
+    VkResult result = vkCreateShaderModule(GetVulkanDeviceManager().GetDeviceChecked(), &createInfo, nullptr, &shaderModule);
+    if (result != VK_SUCCESS) 
+    {
+        Log(Error, "Vulkan", "Failed to create shader module. Result code : %d", result);
+        throw std::runtime_error("Failed to create shader module");
+    }
+    
+    return shaderModule;
+}
+
+VulkanShaderStages VulkanPipelineUtils::CreateShaderStages(const VulkanPipelineInfo& pipelineInfo) 
+{
+    std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
+
+    if (auto vertexShaderModule = CreateShaderModule(pipelineInfo.vertexShaderInfo); vertexShaderModule != VK_NULL_HANDLE)
+    {
+        VkPipelineShaderStageCreateInfo vertexShaderStageInfo = {};
+        vertexShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vertexShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+        vertexShaderStageInfo.module = vertexShaderModule;
+        vertexShaderStageInfo.pName = "main";
+
+        shaderStages.push_back(vertexShaderStageInfo);
+    }
+
+    if (auto tessControlShaderModule = CreateShaderModule(pipelineInfo.tessControlShaderInfo); tessControlShaderModule != VK_NULL_HANDLE)
+    {
+        VkPipelineShaderStageCreateInfo tessControlShaderStageInfo = {};
+        tessControlShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        tessControlShaderStageInfo.stage = VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT;
+        tessControlShaderStageInfo.module = tessControlShaderModule;
+        tessControlShaderStageInfo.pName = "main";
+
+        shaderStages.push_back(tessControlShaderStageInfo);
+    }
+
+    if (auto tessEvalShaderModule = CreateShaderModule(pipelineInfo.tessEvalShaderInfo); tessEvalShaderModule != VK_NULL_HANDLE)
+    {
+        VkPipelineShaderStageCreateInfo tessEvalShaderStageInfo = {};
+        tessEvalShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        tessEvalShaderStageInfo.stage = VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT;
+        tessEvalShaderStageInfo.module = tessEvalShaderModule;
+        tessEvalShaderStageInfo.pName = "main";
+
+        shaderStages.push_back(tessEvalShaderStageInfo);
+    }
+
+    if (auto geometryShaderModule = CreateShaderModule(pipelineInfo.geometryShaderInfo); geometryShaderModule != VK_NULL_HANDLE)
+    {
+        VkPipelineShaderStageCreateInfo geometryShaderStageInfo = {};
+        geometryShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        geometryShaderStageInfo.stage = VK_SHADER_STAGE_GEOMETRY_BIT;
+        geometryShaderStageInfo.module = geometryShaderModule;
+        geometryShaderStageInfo.pName = "main";
+
+        shaderStages.push_back(geometryShaderStageInfo);
+    }
+
+    if (auto fragmentShaderModule = CreateShaderModule(pipelineInfo.fragmentShaderInfo); fragmentShaderModule != VK_NULL_HANDLE)
+    {
+        VkPipelineShaderStageCreateInfo fragmentShaderStageInfo = {};
+        fragmentShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        fragmentShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        fragmentShaderStageInfo.module = fragmentShaderModule;
+        fragmentShaderStageInfo.pName = "main";
+
+        shaderStages.push_back(fragmentShaderStageInfo);
+    }        
+
+    VulkanShaderStages result{};
+    result.shaderStageCount = static_cast<uint8_t>(shaderStages.size());
+    result.shaderStageInfos = (VkPipelineShaderStageCreateInfo*)malloc(sizeof(VkPipelineShaderStageCreateInfo) * result.shaderStageCount);  
+
+    std::copy(shaderStages.begin(), shaderStages.end(), result.shaderStageInfos);
+
+    if (result.shaderStageCount == 0)
+    {
+        Log(Warning, "Vulkan", "No shader stage created");
+    }
+
+    return result;
+}
+
+void VulkanPipelineUtils::DestroyShaderStages(VulkanShaderStages& shaderStageResult) 
+{
+    if (shaderStageResult.shaderStageInfos != nullptr) 
+    {
+        for (uint8_t i = 0; i < shaderStageResult.shaderStageCount; ++i) 
+        {
+            vkDestroyShaderModule(GetVulkanDeviceManager().GetDeviceChecked(), shaderStageResult.shaderStageInfos[i].module, nullptr);
+        }
+
+        free(shaderStageResult.shaderStageInfos);
+        shaderStageResult.shaderStageInfos = nullptr;
+    }
+}
--- src/modules/VulkanAPI/private/Helper/Pipeline/VulkanPipelineUtils.h
+++ src/modules/VulkanAPI/private/Helper/Pipeline/VulkanPipelineUtils.h
@@ -1,17 +1,20 @@
 #ifndef PIPELINEUTILS_H
 #define PIPELINEUTILS_H
 
+#include <vulkan/vulkan.h>
+
+#include "IVulkanMaterial.h"
+
+// TODO: Remove these includes after deleting the deprecated functions
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-#include <vulkan/vulkan.h>
 
-#include "IVulkanMaterial.h"
 #include "VulkanDeviceManager.h"
-#include "Logger.h"
 
 //TODO: Move this function in another module, VulkanAPI is not the right place for it
+[[deprecated]]
 inline std::vector<char> ReadFile(const std::string &filename)
 {
     try
@@ -43,6 +46,7 @@ inline std::vector<char> ReadFile(const std::string &filename)
     }
 }
 
+[[deprecated]]
 inline void InitShaderModule(VkShaderModule *shaderModule, const std::vector<char>& code) {
     VkShaderModuleCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
@@ -56,128 +60,46 @@ inline void InitShaderModule(VkShaderModule *shaderModule, const std::vector<cha
 }
 
 
-
+/**
+ * @namespace VulkanPipelineUtils
+ * @brief Namespace for Vulkan pipeline utilities
+ * 
+ * @author Vectem
+ */
 namespace VulkanPipelineUtils
 {
-    inline VkShaderModule CreateShaderModule(const VulkanShaderInfo& shaderInfo) 
-    {
-        VkShaderModule shaderModule = VK_NULL_HANDLE;
-
-        if (shaderInfo.shaderCode == nullptr || shaderInfo.shaderCodeSize <= 0) 
-        {
-            return VK_NULL_HANDLE;
-        }
-
-        VkShaderModuleCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-        createInfo.codeSize = shaderInfo.shaderCodeSize;
-        createInfo.flags = shaderInfo.flags;
-        createInfo.pCode = reinterpret_cast<uint32_t*>(shaderInfo.shaderCode);
-        createInfo.pNext = nullptr;
-
-        VkResult result = vkCreateShaderModule(GetVulkanDeviceManager().GetDeviceChecked(), &createInfo, nullptr, &shaderModule);
-        if (result != VK_SUCCESS) 
-        {
-            Log(Error, "Vulkan", "Failed to create shader module. Result code : %d", result);
-            throw std::runtime_error("Failed to create shader module");
-        }
-        
-        return shaderModule;
-    }
-
+    /**
+     * @brief Structure to hold shader stages create info
+     * @details This structure holds the count and the array of shader stage create info structures.
+     */
     struct VulkanShaderStages
     {
         uint8_t shaderStageCount{0};
-
         VkPipelineShaderStageCreateInfo* shaderStageInfos{nullptr};
     };
 
-    inline VulkanShaderStages CreateShaderStages(const VulkanPipelineInfo& pipelineInfo) 
-    {
-        std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
+    /**
+     * @brief Create a Shader Module object
+     * 
+     * @param shaderInfo The shader info structure containing the shader code and its size
+     * @return The created shader module 
+     */
+    VkShaderModule CreateShaderModule(const VulkanShaderInfo& shaderInfo);
 
-        if (auto vertexShaderModule = CreateShaderModule(pipelineInfo.vertexShaderInfo); vertexShaderModule != VK_NULL_HANDLE)
-        {
-            VkPipelineShaderStageCreateInfo vertexShaderStageInfo = {};
-            vertexShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-            vertexShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
-            vertexShaderStageInfo.module = vertexShaderModule;
-            vertexShaderStageInfo.pName = "main";
+    /**
+     * @brief Create shader stages from the given pipeline info
+     * 
+     * @param pipelineInfo The pipeline info structure containing the shader info for each stage
+     * @return The created shader stages
+     */
+    VulkanShaderStages CreateShaderStages(const VulkanPipelineInfo& pipelineInfo);
 
-            shaderStages.push_back(vertexShaderStageInfo);
-        }
-
-        if (auto tessControlShaderModule = CreateShaderModule(pipelineInfo.tessControlShaderInfo); tessControlShaderModule != VK_NULL_HANDLE)
-        {
-            VkPipelineShaderStageCreateInfo tessControlShaderStageInfo = {};
-            tessControlShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-            tessControlShaderStageInfo.stage = VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT;
-            tessControlShaderStageInfo.module = tessControlShaderModule;
-            tessControlShaderStageInfo.pName = "main";
-
-            shaderStages.push_back(tessControlShaderStageInfo);
-        }
-
-        if (auto tessEvalShaderModule = CreateShaderModule(pipelineInfo.tessEvalShaderInfo); tessEvalShaderModule != VK_NULL_HANDLE)
-        {
-            VkPipelineShaderStageCreateInfo tessEvalShaderStageInfo = {};
-            tessEvalShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-            tessEvalShaderStageInfo.stage = VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT;
-            tessEvalShaderStageInfo.module = tessEvalShaderModule;
-            tessEvalShaderStageInfo.pName = "main";
-
-            shaderStages.push_back(tessEvalShaderStageInfo);
-        }
-
-        if (auto geometryShaderModule = CreateShaderModule(pipelineInfo.geometryShaderInfo); geometryShaderModule != VK_NULL_HANDLE)
-        {
-            VkPipelineShaderStageCreateInfo geometryShaderStageInfo = {};
-            geometryShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-            geometryShaderStageInfo.stage = VK_SHADER_STAGE_GEOMETRY_BIT;
-            geometryShaderStageInfo.module = geometryShaderModule;
-            geometryShaderStageInfo.pName = "main";
-
-            shaderStages.push_back(geometryShaderStageInfo);
-        }
-
-        if (auto fragmentShaderModule = CreateShaderModule(pipelineInfo.fragmentShaderInfo); fragmentShaderModule != VK_NULL_HANDLE)
-        {
-            VkPipelineShaderStageCreateInfo fragmentShaderStageInfo = {};
-            fragmentShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-            fragmentShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-            fragmentShaderStageInfo.module = fragmentShaderModule;
-            fragmentShaderStageInfo.pName = "main";
-
-            shaderStages.push_back(fragmentShaderStageInfo);
-        }        
-
-        VulkanShaderStages result{};
-        result.shaderStageCount = static_cast<uint8_t>(shaderStages.size());
-        result.shaderStageInfos = (VkPipelineShaderStageCreateInfo*)malloc(sizeof(VkPipelineShaderStageCreateInfo) * result.shaderStageCount);  
-
-        std::copy(shaderStages.begin(), shaderStages.end(), result.shaderStageInfos);
-
-        if (result.shaderStageCount == 0)
-        {
-            Log(Warning, "Vulkan", "No shader stage created");
-        }
-
-        return result;
-    }
-
-    inline void DestroyShaderStages(VulkanShaderStages& shaderStageResult) 
-    {
-        if (shaderStageResult.shaderStageInfos != nullptr) 
-        {
-            for (uint8_t i = 0; i < shaderStageResult.shaderStageCount; ++i) 
-            {
-                vkDestroyShaderModule(GetVulkanDeviceManager().GetDeviceChecked(), shaderStageResult.shaderStageInfos[i].module, nullptr);
-            }
-
-            free(shaderStageResult.shaderStageInfos);
-            shaderStageResult.shaderStageInfos = nullptr;
-        }
-    }
+    /**
+     * @brief Destroy shader stages and free the memory allocated for them
+     * 
+     * @param shaderStageResult The shader stages to destroy
+     */
+    void DestroyShaderStages(VulkanShaderStages& shaderStageResult);
 }
 
 
